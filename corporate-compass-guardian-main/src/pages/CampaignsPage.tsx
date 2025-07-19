ea0f195f42ac7690e87347d205f4ffd446b31925
Roshni Sen <159696584+senroshni19@users.noreply.github.com>
2025-07-19 21:42:17 +0530

    statuscode

--- corporate-compass-guardian-main/src/pages/CampaignsPage.tsx
+++ corporate-compass-guardian-main/src/pages/CampaignsPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Sidebar } from '@/components/Sidebar';
-
+import { EmailCampaigns } from '@/components/EmailCampaigns';
 
 const CampaignsPage = () => {
   return (
