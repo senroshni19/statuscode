56f94746d8da5e8ad28f04210d9c535c968c1310
Roshni Sen <159696584+senroshni19@users.noreply.github.com>
2025-08-03 23:56:38 +0530

    index/2

--- corporate-compass-guardian-main/src/pages/Index.tsx
+++ corporate-compass-guardian-main/src/pages/Index.tsx
@@ -9,7 +9,7 @@ const Index = () => {
       <Sidebar />
       <main className="flex-1 overflow-auto">
         <div className="p-8">
-         
+          <Dashboard />
         </div>
       </main>
     </div>
